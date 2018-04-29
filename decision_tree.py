f377684fed04a454e3ef3b59bf77056fe7390554
Maddie Zug <maddiezug@Maddies-MacBook-Pro-2.local>
2018-04-28 17:20:28 -0700

    initial commit

---
+++ decision_tree.py
@@ -0,0 +1,170 @@
+#
+# Maddie Zug
+# Geology 121 Final project
+#
+
+import numpy as np            
+import pandas as pd
+
+from sklearn import tree      # for decision trees
+from sklearn import ensemble  # for random forests
+
+try: # different imports for different versions of scikit-learn
+    from sklearn.model_selection import cross_val_score   # simpler cv this week
+except ImportError:
+    try:
+        from sklearn.cross_validation import cross_val_score
+    except:
+        print("No cross_val_score!")
+        
+
+#
+# Here are the correct answers to the csv's "unknown" flowers
+#
+answers = ([1]*20)+([0]*10)
+
+
+
+print("+++ Start of pandas' datahandling +++\n")
+
+# df is a "dataframe":
+df = pd.read_csv('data/landslide_data_with_flowacc.csv', sep=',', header=0)   # read the file w/header row #0
+
+# Now, let's take a look at a bit of the dataframe, df:
+df.head()                                 # first five lines
+df.info()                                 # column details
+
+# One important feature is the conversion from string to numeric datatypes!
+# For _input_ features, numpy and scikit-learn need numeric datatypes
+# You can define a transformation function, to help out...
+def transform(s):
+    """ from string to number
+          setosa -> 0
+          versicolor -> 1
+          virginica -> 2
+    """
+    d = { 'unknown':-1, 'setosa':0, 'versicolor':1, 'virginica':2 }
+    return d[s]
+    
+# 
+# this applies the function transform to a whole column
+#
+# df['irisname'] = df['irisname'].map(transform)  # apply the function to the column
+
+print("\n+++ End of pandas +++\n")
+
+print("+++ Start of numpy/scikit-learn +++\n")
+
+print("     +++++ Decision Trees +++++\n\n")
+
+# Data needs to be in numpy arrays - these next two lines convert to numpy arrays
+X_all = df.iloc[:,1:4].values        # iloc == "integer locations" of rows/cols
+y_all = df[ 'LANDSLIDE' ].values      # individually addressable columns (by name)
+
+X_labeled = X_all[31:,:]  # make the 10 into 0 to keep all of the data
+y_labeled = y_all[31:]    # same for this line
+
+#
+# we can scramble the data - but only the labeled data!
+# 
+indices = np.random.permutation(len(X_labeled))  # this scrambles the data each time
+X_data_full = X_labeled[indices]
+y_data_full = y_labeled[indices]
+
+X_train = X_data_full
+y_train = y_data_full
+
+#
+# some labels to make the graphical trees more readable...
+#
+print("Some labels for the graphical tree:")
+feature_names = ['SLOPE', 'ELEVATION', 'FLOWACC']
+target_names = ['landslide', 'no landslide']
+
+#
+# show the creation of three tree files (at three max_depths)
+#
+for max_depth in [1,2,3]:
+    # the DT classifier
+    dtree = tree.DecisionTreeClassifier(max_depth=max_depth)
+
+    # train it (build the tree)
+    dtree = dtree.fit(X_train, y_train) 
+
+    # write out the dtree to tree.dot (or another filename of your choosing...)
+    filename = 'tree' + str(max_depth) + '.dot'
+    tree.export_graphviz(dtree, out_file=filename,   # the filename constructed above...!
+                            feature_names=feature_names,  filled=True, 
+                            rotate=False, # LR vs UD
+                            class_names=target_names, 
+                            leaves_parallel=True )  # lots of options!
+    #
+    # Visualize the resulting graphs (the trees) at www.webgraphviz.com
+    #
+    print("Wrote the file", filename)  
+    #
+
+
+#
+# cross-validation and scoring to determine parameter: max_depth
+# 
+for max_depth in range(1,12):
+    # create our classifier
+    dtree = tree.DecisionTreeClassifier(max_depth=max_depth)
+    #
+    # cross-validate to tune our model (this week, all-at-once)
+    #
+    scores = cross_val_score(dtree, X_train, y_train, cv=5)
+    average_cv_score = scores.mean()
+    print("For depth=", max_depth, "average CV score = ", average_cv_score)  
+    # print("      Scores:", scores)
+
+# import sys
+# print("bye!")
+# sys.exit(0)
+
+MAX_DEPTH = 3   # choose a MAX_DEPTH based on cross-validation... 
+print("\nChoosing MAX_DEPTH =", MAX_DEPTH, "\n")
+
+#
+# now, train the model with ALL of the training data...  and predict the unknown labels
+#
+
+X_unknown = X_all[:30,:4]              # the final testing data
+X_train = X_all[31:,:4]              # the training data
+
+y_unknown = y_all[:30]                  # the final testing outputs/labels (unknown)
+y_train = y_all[31:]                  # the training outputs/labels (known)
+
+# our decision-tree classifier...
+dtree = tree.DecisionTreeClassifier(max_depth=MAX_DEPTH)
+dtree = dtree.fit(X_train, y_train) 
+
+#
+# and... Predict the unknown data labels
+#
+print("Decision-tree predictions:\n")
+predicted_labels = dtree.predict(X_unknown)
+answer_labels = answers
+
+#
+# formatted printing! (docs.python.org/3/library/string.html#formatstrings)
+#
+s = "{0:<11} | {1:<11}".format("Predicted","Answer")
+#  arg0: left-aligned, 11 spaces, string, arg1: ditto
+print(s)
+s = "{0:<11} | {1:<11}".format("-------","-------")
+print(s)
+# the table...
+for p, a in zip( predicted_labels, answer_labels ):
+    s = "{0:<11} | {1:<11}".format(p,a)
+    print(s)
+
+#
+# feature importances!
+#
+print()
+print("dtree.feature_importances_ are\n      ", dtree.feature_importances_) 
+print("Order:", feature_names[1:4])
+
+
